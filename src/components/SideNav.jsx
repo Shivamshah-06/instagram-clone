aaa25744194c5226fc1355cb089f549a0dd552c4
Shivamshah-06 <shivamshah93865@gmail.com>
2024-02-06 22:31:48 +0530

    dffd

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -15,7 +15,7 @@ import mypic from "../images/mypic.jpg"
 const SideNav = () => {
   return (
     <div>
-      <div className="d-flex flex-column flex-shrink-0 p-3 left bg-black text-bg-dark" style={{width: "220px"}}>
+      <div className="d-flex flex-column flex-shrink-0 p-3 left bg-black text-bg-dark" style={{width: "220px", height:"100vh"}}>
     <a href="/" className="d-flex align-items-center mb-1 mb-md-0 me-md-auto text-white text-decoration-none">
       <img src={insta} alt="" style={{height:"80px", width:"150px"}} />
     </a>
