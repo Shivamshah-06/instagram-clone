34f2dc6cb5c376fb3b1c36c013096497b554896e
Shivamshah-06 <shivamshah93865@gmail.com>
2024-02-06 22:25:40 +0530

    hello

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,52 +1,88 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/js/index.esm"
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import ExploreIcon from '@mui/icons-material/Explore';
+import SlowMotionVideoIcon from '@mui/icons-material/SlowMotionVideo';
+import SendIcon from '@mui/icons-material/Send';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import threads from "../images/threads.png";
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
+import insta from "../images/hero_logo.png";
+import mypic from "../images/mypic.jpg"
 const SideNav = () => {
   return (
     <div>
-      <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style={{width: "280px"}}>
-    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <svg className="bi pe-none me-2" style={{width:"40", height:"32"}}><use xlinkHref="#bootstrap"></use></svg>
-      <span className="fs-4">Sidebar</span>
+      <div className="d-flex flex-column flex-shrink-0 p-3 left bg-black text-bg-dark" style={{width: "220px"}}>
+    <a href="/" className="d-flex align-items-center mb-1 mb-md-0 me-md-auto text-white text-decoration-none">
+      <img src={insta} alt="" style={{height:"80px", width:"150px"}} />
     </a>
-    <hr/>
     <ul className="nav nav-pills flex-column mb-auto">
-      <li className="nav-item">
-        <a href="#" className="nav-link active" aria-current="page">
-          <svg className="bi pe-none me-2" style={{width:"16", height:"16"}}><use xlinkHref="#home"></use></svg>
+      <li className="nav-item ">
+        <a href="#" className="nav-link text-white " aria-current="page">
+        <HomeIcon className='mx-2'/>
           Home
         </a>
       </li>
-      <li>
-        <a href="#" className="nav-link text-white">
-          <svg className="bi pe-none me-2" style={{width:"16", height:"16"}}><use xlinkHref="#speedometer2"></use></svg>
-          Dashboard
+      <li className="nav-item">
+        <a href="#" className="nav-link text-white " aria-current="page">
+        <SearchIcon className='mx-2'/>
+         Search
         </a>
       </li>
-      <li>
-        <a href="#" className="nav-link text-white">
-          <svg className="bi pe-none me-2" style={{width:"16", height:"16"}}><use xlinkHref="#table"></use></svg>
-          Orders
+      <li className="nav-item">
+        <a href="#" className="nav-link text-white " aria-current="page">
+        <ExploreIcon className='mx-2'/>
+          Explore
         </a>
       </li>
-      <li>
-        <a href="#" className="nav-link text-white">
-          <svg className="bi pe-none me-2" style={{width:"16", height:"16"}}><use xlinkHref="#grid"></use></svg>
-          Products
+      <li className="nav-item">
+        <a href="#" className="nav-link text-white " aria-current="page">
+        <SlowMotionVideoIcon className='mx-2'/>
+        Reels
         </a>
       </li>
-      <li>
-        <a href="#" className="nav-link text-white">
-          <svg className="bi pe-none me-2" style={{width:"16", height:"16"}}><use xlinkHref="#people-circle"></use></svg>
-          Customers
+      <li className="nav-item">
+        <a href="#" className="nav-link text-white " aria-current="page">
+          <SendIcon className='mx-2'/>
+          Message
         </a>
       </li>
-    </ul>
-    <hr/>
-    <div className="dropdown">
-      <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" style={{width:"6", height:"6"}} className="rounded-circle me-2"/>
-        <strong>mdo</strong>
+      <li className="nav-item">
+        <a href="#" className="nav-link text-white " aria-current="page">
+          <FavoriteBorderIcon className='mx-2'/>
+          Notifications
+        </a>
+      </li>
+      <li className="nav-item">
+        <a href="#" className="nav-link text-white " aria-current="page">
+          <AddCircleOutlineIcon className='mx-2'/>
+          Create
+          
+        </a>
+      </li>
+      
+
+      <li className="nav-item">
+        <a href="#" className="nav-link text-white " aria-current="page">
+          <img src={mypic} alt="" style={{width:"22px", borderRadius:"30px", marginLeft:"6px" ,marginRight:"14px"}} />
+          Profile
+        </a>
+      </li>
+      <br />
+      <li className="nav-item">
+        <a href="#" className="nav-link text-white " aria-current="page">
+        <img src={threads} alt="" style={{width:"32px", borderRadius:"10px", marginLeft:"0px"}}/>
+        Threads
+        </a>
+      </li>
+      <li className="nav-item">
+        <div className="dropdown">
+        <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false">
+        
+        <strong><DensityMediumIcon style={{marginLeft:"20px", marginRight:"6px"}}/>More</strong>
       </a>
       <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
         <li><a className="dropdown-item" href="#">New project...</a></li>
@@ -55,7 +91,12 @@ const SideNav = () => {
         <li><hr className="dropdown-divider"/></li>
         <li><a className="dropdown-item" href="#">Sign out</a></li>
       </ul>
-    </div>
+        </div>
+      </li>
+      
+    </ul>
+    <hr/>
+   
   </div>
     </div>
   )
